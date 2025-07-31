b34ecd6b2dcc47532a91dc8519da5588a6ea0180
Simon Gamma <github@survive.ch>
2025-07-31 16:48:13 +0200

    spotless-cli v0.2.0

--- spotless-cli/tools/chocolateyinstall.ps1
+++ spotless-cli/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.18.0 at 2025-06-02T19:14:01.5480517Z
+# Generated with JReleaser 1.19.0 at 2025-07-31T14:40:55.5302165Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
 
 Install-ChocolateyZipPackage `
     -PackageName 'spotless-cli' `
-    -Url 'https://github.com/diffplug/spotless-cli/releases/download/v0.1.1/spotless-0.1.1-windows-x86_64.zip' `
-    -Checksum 'c7aa2231517b539bf6a52a6da6737a16c7cbbb1e2cca7d17d6825f0cc1ea08a2' `
+    -Url 'https://github.com/diffplug/spotless-cli/releases/download/v0.2.0/spotless-0.2.0-windows-x86_64.zip' `
+    -Checksum '6a7255b48ec4bc7db96c4313cfb525bceec55a3863416cdf1e65417998487981' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
